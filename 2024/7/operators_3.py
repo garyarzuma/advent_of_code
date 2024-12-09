27e3264df0809dafba0b2c316dfd03fb782cacf3
Gary Arzumanyan <garyarzumanyan@gmail.com>
2024-12-08 21:31:11 -0800

    day 6 part 1, 2 w bug, day 7 solution

---
+++ 2024/7/operators_3.py
@@ -0,0 +1,34 @@
+import fileinput
+
+input = fileinput.input('input')
+
+mySum = 0
+
+
+def next(prev, nums, i, ans, ops):
+
+    if prev == ans:
+        return True
+    if i == len(nums):
+        return False
+
+    prev1 = prev * nums[i]
+    prev2 = prev + nums[i]
+    prev3 = int(str(prev) + str(nums[i]))
+
+    return (next(prev1, nums, i+1, ans, ops + '*') or
+            next(prev2, nums, i+1, ans, ops + '+') or
+            next(prev3, nums, i+1, ans, ops + '|'))
+
+
+for line in input:
+    ans, nums = line.split(':')
+    ans = int(ans)
+
+    nums = nums.lstrip(' ').rstrip('\n').split(' ')
+    nums = list(map(int, nums))
+
+    if next(nums[0], nums, 1, ans, ''):
+        mySum += ans
+
+print(mySum)
